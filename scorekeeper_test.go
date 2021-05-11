fced3d2f66270e6df72994a1969118bcd23a6fae
ben <benhdev@gmail.com>
2021-05-11 13:28:58 -0400

    Adds TestGetStats
    and some preliminary test cases
    also small formatting fix to error messages in tests

--- scorekeeper_test.go
+++ scorekeeper_test.go
@@ -69,8 +69,101 @@ func TestAddAction(t *testing.T) {
 		s := ScoreKeeper{}
 
 		err := s.AddAction(tc.action)
-		if err != tc.err {
-			t.Errorf("[%s] Expected error to be %v but got %v", tc.name, tc.err, err)
+		if expected, got := tc.err, err; expected != got {
+			t.Errorf("[%s] Expected error to be '%v' but got '%v'", tc.name, expected, got)
+		}
+	}
+}
+
+func TestGetStats(t *testing.T) {
+	type testCase struct {
+		name    string
+		actions []string
+		stats   string
+		errs    []error
+	}
+	testCases := []testCase{
+		{
+			name: "provided",
+			actions: []string{
+				`{"action":"jump", "time":100}`,
+				`{"action":"run", "time":75}`,
+				`{"action":"jump", "time":200}`,
+			},
+			stats: `[
+				{"action":"jump", "avg":150},
+				{"action":"run", "avg":75}
+			]`,
+			errs: []error{nil, nil, nil},
+		},
+		{
+			name:    "empty",
+			actions: []string{},
+			stats:   `[]`,
+			errs:    []error{},
+		},
+		{
+			name: "zero",
+			actions: []string{
+				`{"action":"stand", "time":0}`,
+			},
+			stats: `[
+				{"action":"stand", "avg":0}
+			]`,
+			errs: []error{nil},
+		},
+		{
+			name: "unique",
+			actions: []string{
+				`{"action":"hop", "time":1}`,
+				`{"action":"skip", "time":2}`,
+				`{"action":"jump", "time":3}`,
+			},
+			stats: `[
+				{"action":"hop", "time":1},
+            	{"action":"skip", "time":2},
+            	{"action":"jump", "time":3}
+			]`,
+			errs: []error{nil, nil, nil},
+		},
+		{
+			name: "negative",
+			actions: []string{
+				`{"action":"sink", "time":-100}`,
+			},
+			stats: `[]`,
+			errs:  []error{ErrOutOfBounds},
+		},
+		{
+			name: "robust",
+			actions: []string{
+				`{"action":"sink", "time":-100}`,
+				`{"action":"exist"}`,
+				`{"action":"jump", "time":100}`,
+				`{"action":"run", "time":75}`,
+				`{"action":"jump", "time":200}`,
+			},
+			stats: `[
+				{"action":"jump", "avg":150},
+				{"action":"run", "avg":75}
+			]`,
+			errs: []error{ErrOutOfBounds, ErrBadInput, nil, nil, nil},
+		},
+	}
+
+	for _, tc := range testCases {
+		s := ScoreKeeper{}
+
+		for i, a := range tc.actions {
+			errs := make([]error, len(tc.actions))
+			errs[i] = s.AddAction(a)
+			if expected, got := tc.errs[i], errs[i]; expected != got {
+				t.Errorf("[%s] Expected error for action %d to be '%v' but got '%v'", tc.name, i, expected, got)
+			}
+		}
+
+		if expected, got := tc.stats, s.GetStats(); expected != got {
+			t.Errorf("[%s] Expected stats to be '%s' but got '%s'", tc.name, expected, got)
 		}
 	}
 }
