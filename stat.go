639fccb3ec4f4c398e48689e6fbc9124fec04d64
ben <benhdev@gmail.com>
2021-05-12 19:27:08 -0400

    Adds Average stat
    Adds the average stat that will be the only stat used in this project. It only works on scores with int64 values to avoid precision issues when type converting to float64 for math

--- stat.go
+++ stat.go
@@ -1,5 +1,10 @@
 package scorekeeper
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Stat does some math on a set of scores and returns the result as a string
 // We will only have one kind of Statistic for this project (Average),
 // but through this interface we could extend to other kinds easily.
@@ -11,4 +16,60 @@ type Stat interface {
 	Step(s Score) error
 	// Report returns the result of the running computation.
 	Report() (string, error)
+}
+
+// Average is a Stat that computes an average of scores with int64 values.
+type Average struct {
+	n float64
+	s float64
+}
+
+var ErrTypeInvalid = errors.New("score value is invalid")
+var ErrNoData = errors.New("no data to report")
+
+// Compute a floating point average from a list of scores with int64 values and return the result as a string.
+func (a *Average) Compute(ss []Score) (string, error) {
+	var (
+		sum float64
+		n   float64
+	)
+
+	for _, s := range ss {
+		v, ok := s.Value().(int64)
+		if !ok {
+			return "", ErrTypeInvalid
+		}
+		sum += float64(v)
+		n++
+	}
+
+	if n < float64(1) {
+		return "", ErrNoData
+	}
+
+	avg := sum / n
+
+	return fmt.Sprint(avg), nil
+}
+
+// Step adds a score to the running sum.
+func (a *Average) Step(s Score) error {
+	v, ok := s.Value().(int64)
+	if !ok {
+		return ErrTypeInvalid
+	}
+
+	a.s += float64(v)
+	a.n++
+	return nil
+}
+
+// Report the average from the running sum as a string.
+func (a *Average) Report() (string, error) {
+	if a.n < float64(1) {
+		return "", ErrNoData
+	}
+
+	avg := a.s / a.n
+	return fmt.Sprint(avg), nil
 }
---
+++ stat_test.go
@@ -0,0 +1,120 @@
+package scorekeeper
+
+import (
+	"testing"
+)
+
+type avgTestCase struct {
+	name string
+	ss   []Score
+	errs []error
+	res  string
+	err  error
+}
+
+var testCases = []avgTestCase{
+	{
+		name: "provided",
+		ss: []Score{
+			&TestScore{value: int64(100)},
+			&TestScore{value: int64(200)},
+		},
+		errs: []error{nil, nil},
+		res:  "150",
+	},
+	{
+		name: "empty",
+		ss:   []Score{},
+		errs: []error{},
+		res:  "",
+		err:  ErrNoData,
+	},
+	{
+		name: "zero",
+		ss: []Score{
+			&TestScore{name: "jump", value: int64(0)},
+			&TestScore{name: "jump", value: int64(200)},
+		},
+		errs: []error{nil, nil},
+		res:  "100",
+	},
+	{
+		name: "one",
+		ss: []Score{
+			&TestScore{name: "jump", value: int64(1)},
+		},
+		errs: []error{nil},
+		res:  "1",
+	},
+	{
+		// Presumably not possible for Trial but just in case
+		name: "negative",
+		ss: []Score{
+			&TestScore{name: "jump", value: int64(-200)},
+			&TestScore{name: "jump", value: int64(200)},
+		},
+		errs: []error{nil, nil},
+		res:  "0",
+	},
+	{
+		name: "duplicate",
+		ss: []Score{
+			&TestScore{name: "jump", value: int64(100)},
+			&TestScore{name: "jump", value: int64(100)},
+		},
+		errs: []error{nil, nil},
+		res:  "100",
+	},
+	{
+		name: "floating",
+		ss: []Score{
+			&TestScore{name: "jump", value: int64(101)},
+			&TestScore{name: "jump", value: int64(100)},
+		},
+		errs: []error{nil, nil},
+		res:  "100.5",
+	},
+	{
+		name: "repeating of course",
+		ss: []Score{
+			&TestScore{name: "jump", value: int64(2)},
+			&TestScore{name: "jump", value: int64(3)},
+			&TestScore{name: "jump", value: int64(5)},
+		},
+		errs: []error{nil, nil, nil},
+		res:  "3.3333333333333335",
+	},
+}
+
+func TestAverage(t *testing.T) {
+	for _, tc := range testCases {
+		a := Average{}
+
+		for i, s := range tc.ss {
+			if expected, got := tc.errs[i], a.Step(s); expected != got {
+				t.Errorf("[%s] Expected error to be '%v' but got '%v'", tc.name, expected, got)
+			}
+		}
+
+		res, err := a.Report()
+		if expected, got := tc.err, err; expected != got {
+			t.Errorf("[%s] Expected error to be '%v' but got '%v'", tc.name, expected, got)
+		}
+		if expected, got := tc.res, res; expected != got {
+			t.Errorf("[%s] Expected %s but got %s", tc.name, expected, got)
+		}
+
+		res2, err := a.Compute(tc.ss)
+		if expected, got := tc.err, err; expected != got {
+			t.Errorf("[%s] Compute: Expected error to be '%v' but got '%v'", tc.name, expected, got)
+		}
+		if expected, got := tc.res, res2; expected != got {
+			t.Errorf("[%s] Compute: Expected %s but got %s", tc.name, expected, got)
+		}
+
+		// sanity check
+		if res != res2 {
+			t.Errorf("[%s] results didn't match: running: %s, computed: %s", tc.name, res, res2)
+		}
+	}
+}
