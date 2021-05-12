89938011aff4d11b3bf34b7679a4294401c381f5
ben <benhdev@gmail.com>
2021-05-11 21:37:18 -0400

    Mediate the store with Keep
    This way we can initialize the store if it was uninitialized. default to Memory Store

--- scorekeeper.go
+++ scorekeeper.go
@@ -2,14 +2,16 @@ package scorekeeper
 
 // ScoreKeeper keeps scores using some store.
 // It is the top level object for the ScoreKeeper library.
+// It fulfills the requested AddAction and GetStats methods.
 type ScoreKeeper struct {
-	store ScoreStore
+	s ScoreStore
 }
 
 // ScoreStore stores scores for ScoreKeeper.
 // It could be in memory or backed by a database.
 type ScoreStore interface {
-	Store() error
+	Store(s Score) error
+	Retrieve(name string) ([]Score, error)
 }
 
 // Score is kept by ScoreKeeper and tracks something.
@@ -19,12 +21,28 @@ type Score interface {
 	Read(s string) error
 }
 
-func (sk *ScoreKeeper) AddAction(action string) error {
-	var s Trial
-	return s.Read(action)
+// Keep a Score in the ScoreStore, using a memory store if none was specified.
+func (sk *ScoreKeeper) Keep(score Score) error {
+	if sk.s == nil {
+		sk.s = &MemoryStore{
+			s: map[string][]Score{},
+		}
+	}
+
+	return sk.s.Store(score)
 }
 
-// GetStats computes some statistics about the actions stored in the ScoreKeeper
+// AddAction takes a json-encoded string and keeps it for later.
+func (sk *ScoreKeeper) AddAction(action string) error {
+	var s Trial
+	if err := s.Read(action); err != nil {
+		return err
+	}
+
+	return sk.Keep(&s)
+}
+
+// GetStats computes some statistics about the actions stored in the ScoreKeeper.
 func (s *ScoreKeeper) GetStats() string {
 	return ""
 }
