aa4db42654f76c3be19ebb9d19398563d8eaead1
ben <benhdev@gmail.com>
2021-05-13 13:32:00 -0400

    Add an example program

---
+++ example/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+
+	"github.com/bdharris08/scorekeeper"
+)
+
+var (
+	numWorkers = 5
+)
+
+func main() {
+	scoreKeeper, err := scorekeeper.New(&scorekeeper.MemoryStore{})
+	if err != nil {
+		panic(fmt.Errorf("error creating scoreKeeper: %v", err))
+	}
+
+	scoreKeeper.Start()
+	defer scoreKeeper.Stop()
+
+	actions := []string{
+		`{"action":"hop", "time":100}`,
+		`{"action":"skip", "time":100}`,
+		`{"action":"jump", "time":100}`,
+		`{"action":"hop", "time":200}`,
+		`{"action":"skip", "time":200}`,
+		`{"action":"jump", "time":200}`,
+		`{"action":"hop", "time":1}`,
+		`{"action":"hop", "time":1}`,
+		`{"action":"hop", "time":1}`,
+		`{"action":"skip", "time":2}`,
+		`{"action":"skip", "time":2}`,
+		`{"action":"skip", "time":2}`,
+		`{"action":"jump", "time":3}`,
+		`{"action":"jump", "time":3}`,
+		`{"action":"jump", "time":3}`,
+	}
+
+	// A simple example
+	for _, a := range actions {
+		if err := scoreKeeper.AddAction(a); err != nil {
+			fmt.Println(err)
+		}
+	}
+	result, err := scoreKeeper.GetStats()
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Do something with the result
+	fmt.Println(result)
+
+	// You can even access the scoreKeeper concurrently!
+	var wg sync.WaitGroup
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			for _, a := range actions {
+				if err := scoreKeeper.AddAction(a); err != nil {
+					fmt.Println(err)
+				}
+			}
+			result, err := scoreKeeper.GetStats()
+			if err != nil {
+				fmt.Println(err)
+			}
+			// Do something with the result
+			fmt.Println(result)
+		}()
+	}
+
+	wg.Wait()
+
+	result, err = scoreKeeper.GetStats()
+	if err != nil {
+		fmt.Println(err)
+	}
+	// Do something with the result
+	fmt.Println(result)
+}
